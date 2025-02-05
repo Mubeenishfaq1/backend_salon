7abfab1cd1b657df7dde9e3c0fa846d5556f4911
Mubeenishfaq1 <ishfaqmubeen00@gmail.com>
2025-02-06 02:34:16 +0500

    ytyyuaaai

--- connection.js
+++ connection.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const connection = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI ||
-        "mongodb+srv://userex7676:Vnhn8CJte51BwREY@cluster0.z1qdn.mongodb.net/"
+      "mongodb+srv://userex7676:Vnhn8CJte51BwREY@cluster0.z1qdn.mongodb.net/"
     );
     console.log("Connected to MongoDB");
   } catch (error) {
